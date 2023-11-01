175b49282a0f1ba66c8cc4902c0317c3c72f51d3
Pariya <87968419+pariyajebreili@users.noreply.github.com>
2023-11-01 20:39:44 +0330

    enhancement in live sketch code

--- 1.Live_Sketch_Using_Webcam.py
+++ 1.Live_Sketch_Using_Webcam.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
 
-# Our sketch generating function
+#sketch generating function
 def sketch(image):
     # Convert image to grayscale
     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     # Clean up image using Guassian Blur
+    # smooths the image by averaging pixel values in a local neighborhood.
+    # helps in reducing high-frequency noise and small variations in the image, which can be seen as small, unwanted details or fluctuations.
+    # obtain more stable and accurate edge maps.
     img_gray_blur = cv2.GaussianBlur(img_gray, (5,5), 0)
     
     # Extract edges
@@ -21,12 +24,30 @@ def sketch(image):
 # It contains a boolean indicating if it was sucessful (ret)
 # It also contains the images collected from the webcam (frame)
 cap = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(1)
+
+
+if not cap.isOpened() and cap2.isOpened():
+    print("Error: Could not open the webcam.")
+    exit()
+
+
 
 while True:
     ret, frame = cap.read()
-    cv2.imshow('Our Live Sketcher', sketch(frame))
-    if cv2.waitKey(1) == 13: #1 is the Enter Key
+
+    if not ret:
+        print("Error: Could not read frame from the webcam.")
         break
+
+    cv2.imshow('Live_image', (frame))
+    cv2.imshow('Live_Sketch', sketch(frame))
+
+    key = cv2.waitKey(1)
+    if key==ord('q'):      # Press 'q' to quit
+        break
+    elif key == ord('s'):  # Press 's' to save the sketch as an image
+        cv2.imwrite("sketch.png", sketch(frame))
         
 # Release camera and close windows
 cap.release()
